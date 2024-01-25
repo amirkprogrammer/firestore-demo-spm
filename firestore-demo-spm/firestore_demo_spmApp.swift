824c44201a8ea49b417c1fcdfe86ff014fa10f51
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-01-24 21:20:39 -0800

    Initial Commit

---
+++ firestore-demo-spm/firestore_demo_spmApp.swift
@@ -0,0 +1,17 @@
+//
+//  firestore_demo_spmApp.swift
+//  firestore-demo-spm
+//
+//  Created by Amir Kabiri on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct firestore_demo_spmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
